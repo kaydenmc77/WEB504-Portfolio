a94420576c08f4b8b882ddf9e8220332c4599171
Kayden <kayden-mcmanaway@live.nmit.ac.nz>
2024-11-19 12:41:38 +1300

    Started the comment section and then shit broke

---
+++ index.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAJNMbf2UYXiQOOwagkCRrNqeQs-D8tOos",
+  authDomain: "portfolio-caf72.firebaseapp.com",
+  databaseURL: "https://portfolio-caf72-default-rtdb.firebaseio.com",
+  projectId: "portfolio-caf72",
+  storageBucket: "portfolio-caf72.firebasestorage.app",
+  messagingSenderId: "1087473340138",
+  appId: "1:1087473340138:web:93f4e43206de99aab05193",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
